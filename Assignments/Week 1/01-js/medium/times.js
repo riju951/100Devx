af28e9967606583f2a2c6af284a9fa2879ad6c29
Rijans Bhagat <rijans.bahgat@gmail.com>
2024-02-17 17:21:34 +0530

    Week 1 done

---
+++ Assignments/Week 1/01-js/medium/times.js
@@ -0,0 +1,29 @@
+/*
+Write a function that calculates the time (in seconds) it takes for the JS code to calculate sum from 1 to n, given n as the input.
+Try running it for
+1. Sum from 1-100
+2. Sum from 1-100000
+3. Sum from 1-1000000000
+Hint - use Date class exposed in JS
+There is no automated test for this one, this is more for you to understand time goes up as computation goes up
+*/
+
+function calculateTime(n) {
+    // Find the start time.
+    const startTime = new Date().getTime();
+    // To find the sum.
+    let sum = 0;
+    // Calculate sum from 1 to n.
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+    // Find the end time.
+    const endTime = new Date().getTime();
+    // Calculate the time difference between the end time and start time.
+    const elapsedTime = (endTime - startTime) / 1000;
+    return elapsedTime;
+  }
+  
+  console.log(calculateTime(100));
+  console.log(calculateTime(100000));
+  console.log(calculateTime(1000000000));
